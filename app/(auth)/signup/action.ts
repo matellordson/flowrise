755976a85ef51d44d39d3eea230cc6bde9bdd2b0
Matel Lordson <snr.mn.lordson@gmail.com>
2025-06-25 03:14:21 +0000

    commit

--- app/(auth)/signup/action.ts
+++ app/(auth)/signup/action.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function SignupAction(formData: FormData) {
   const email = formData.get("email") as string;
@@ -15,5 +16,5 @@ export default async function SignupAction(formData: FormData) {
     },
   });
 
-  console.log("account created");
+  redirect("/login");
 }
--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -1,5 +1,6 @@
-import { GalleryVerticalEnd } from "lucide-react";
+"use client";
 
+import { GalleryVerticalEnd } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
