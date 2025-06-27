084b028bb76018d59c3355e63abce6a2b0b64c46
Matel Lordson <snr.mn.lordson@gmail.com>
2025-06-27 00:32:38 +0000

    cmt

--- app/(sections)/crypto/deposit/coins/bitcoin.tsx
+++ app/(sections)/crypto/deposit/coins/bitcoin.tsx
@@ -71,179 +71,178 @@ export default function DepositBTC() {
     console.log(values);
   }
 
-  if (isDesktop) {
-    return (
-      <Dialog open={open} onOpenChange={setOpen}>
-        <DialogTrigger asChild>
-          <Button variant="outline">
-            <TokenBTC variant="branded" className="size-5" />
-            Bitcoin
-          </Button>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle>Deposit Bitcoin</DialogTitle>
-          </DialogHeader>
-          {/* alert */}
-          <Alert variant="default">
-            <OctagonAlert />
-            <AlertTitle>Deposit Instructions</AlertTitle>
-            <AlertDescription>
-              Please copy the wallet address and send the coins to it. Once the
-              transaction is confirmed on the network, your deposited coins will
-              appear in your wallet.
-            </AlertDescription>
-          </Alert>
-
-          {/* form */}
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="amount"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Amount</FormLabel>
-                    <FormControl>
-                      <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Minimum amount of Bitcoin is 0.0005.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* copy address */}
-              <div className="">
-                <div className="bg-muted w-full py-2 flex justify-center items-center rounded font-mono relative">
-                  <span className="select-all flex-1 text-center overflow-hidden text-muted-forground text-sm">
-                    <span className="inline-block max-w-full">
-                      <span className="inline">{textToCopy.slice(0, 6)}</span>
-                      <span className="inline text-muted-foreground/60">
-                        ...
-                      </span>
-                      <span className="inline">{textToCopy.slice(-6)}</span>
-                    </span>
-                  </span>
-                  <button
-                    onClick={handleCopy}
-                    className="absolute right-3 p-1 rounded hover:bg-background/80 transition-colors"
-                    title={copied ? "Copied!" : "Copy to clipboard"}
-                  >
-                    {copied ? (
-                      <Check className="h-4 w-4 text-green-600" />
-                    ) : (
-                      <Copy className="h-4 w-4 text-muted-foreground hover:text-foreground" />
-                    )}
-                  </button>
-                </div>
-              </div>
-              <Button type="submit" className="w-full">
-                Make Deposit
-              </Button>
-            </form>
-          </Form>
-        </DialogContent>
-      </Dialog>
-    );
-  }
-
+  // if (isDesktop) {
   return (
-    <Drawer open={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
         <Button variant="outline">
           <TokenBTC variant="branded" className="size-5" />
           Bitcoin
         </Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <DrawerHeader className="text-left">
-          <DrawerTitle>Deposit Bitcoin</DrawerTitle>
-        </DrawerHeader>
-        <div className="px-4 space-y-5">
-          {/* alert */}
-          <Alert variant="default">
-            <OctagonAlert />
-            <AlertTitle>Deposit Instructions</AlertTitle>
-            <AlertDescription>
-              Please copy the wallet address and send the coins to it. Once the
-              transaction is confirmed on the network, your deposited coins will
-              appear in your wallet.
-            </AlertDescription>
-          </Alert>
-          {/* form */}
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              <FormField
-                control={form.control}
-                name="amount"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Amount</FormLabel>
-                    <FormControl>
-                      <Input placeholder="shadcn" {...field} />
-                    </FormControl>
-                    <FormDescription>
-                      Minimum amount of Bitcoin is 0.0005.
-                    </FormDescription>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              {/* copy address */}
-              <div className="">
-                <div className="bg-muted w-full py-2 flex justify-center items-center rounded font-mono relative">
-                  <span className="select-all flex-1 text-center overflow-hidden text-muted-forground text-sm">
-                    <span className="inline-block max-w-full">
-                      <span className="inline">{textToCopy.slice(0, 6)}</span>
-                      <span className="inline text-muted-foreground/60">
-                        ...
-                      </span>
-                      <span className="inline">{textToCopy.slice(-6)}</span>
-                    </span>
-                  </span>
-                  <button
-                    onClick={handleCopy}
-                    className="absolute right-3 p-1 rounded hover:bg-background/80 transition-colors"
-                    title={copied ? "Copied!" : "Copy to clipboard"}
-                  >
-                    {copied ? (
-                      <Check className="h-4 w-4 text-green-600" />
-                    ) : (
-                      <Copy className="h-4 w-4 text-muted-foreground hover:text-foreground" />
-                    )}
-                  </button>
-                </div>
-              </div>
-              <Button type="submit" className="w-full">
-                Make Deposit
-              </Button>
-            </form>
-          </Form>
-        </div>
-        <DrawerFooter className="pt-2 pb-5">
-          <DrawerClose asChild>
-            <Button variant="outline">Cancel</Button>
-          </DrawerClose>
-        </DrawerFooter>
-      </DrawerContent>
-    </Drawer>
-  );
-}
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Deposit Bitcoin</DialogTitle>
+        </DialogHeader>
+        {/* alert */}
+        <Alert variant="default">
+          <OctagonAlert />
+          <AlertTitle>Deposit Instructions</AlertTitle>
+          <AlertDescription>
+            Please copy the wallet address and send the coins to it. Once the
+            transaction is confirmed on the network, your deposited coins will
+            appear in your wallet.
+          </AlertDescription>
+        </Alert>
 
-function ProfileForm({ className }: React.ComponentProps<"form">) {
-  return (
-    <form className={cn("grid items-start gap-6", className)}>
-      <div className="grid gap-3">
-        <Label htmlFor="email">Email</Label>
-        <Input type="email" id="email" defaultValue="shadcn@example.com" />
-      </div>
-      <div className="grid gap-3">
-        <Label htmlFor="username">Username</Label>
-        <Input id="username" defaultValue="@shadcn" />
-      </div>
-      <Button type="submit">Save changes</Button>
-    </form>
+        {/* form */}
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+            <FormField
+              control={form.control}
+              name="amount"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Amount</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    Minimum amount of Bitcoin is 0.0005.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            {/* copy address */}
+            <div className="">
+              <div className="bg-muted w-full py-2 flex justify-center items-center rounded font-mono relative">
+                <span className="select-all flex-1 text-center overflow-hidden text-muted-forground text-sm">
+                  <span className="inline-block max-w-full">
+                    <span className="inline">{textToCopy.slice(0, 6)}</span>
+                    <span className="inline text-muted-foreground/60">...</span>
+                    <span className="inline">{textToCopy.slice(-6)}</span>
+                  </span>
+                </span>
+                <button
+                  onClick={handleCopy}
+                  className="absolute right-3 p-1 rounded hover:bg-background/80 transition-colors"
+                  title={copied ? "Copied!" : "Copy to clipboard"}
+                >
+                  {copied ? (
+                    <Check className="h-4 w-4 text-green-600" />
+                  ) : (
+                    <Copy className="h-4 w-4 text-muted-foreground hover:text-foreground" />
+                  )}
+                </button>
+              </div>
+            </div>
+            <Button type="submit" className="w-full">
+              Make Deposit
+            </Button>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 }
+// }
+
+//   return (
+//     <Drawer open={open} onOpenChange={setOpen}>
+//       <DrawerTrigger asChild>
+//         <Button variant="outline">
+//           <TokenBTC variant="branded" className="size-5" />
+//           Bitcoin
+//         </Button>
+//       </DrawerTrigger>
+//       <DrawerContent>
+//         <DrawerHeader className="text-left">
+//           <DrawerTitle>Deposit Bitcoin</DrawerTitle>
+//         </DrawerHeader>
+//         <div className="px-4 space-y-5">
+//           {/* alert */}
+//           <Alert variant="default">
+//             <OctagonAlert />
+//             <AlertTitle>Deposit Instructions</AlertTitle>
+//             <AlertDescription>
+//               Please copy the wallet address and send the coins to it. Once the
+//               transaction is confirmed on the network, your deposited coins will
+//               appear in your wallet.
+//             </AlertDescription>
+//           </Alert>
+//           {/* form */}
+//           <Form {...form}>
+//             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+//               <FormField
+//                 control={form.control}
+//                 name="amount"
+//                 render={({ field }) => (
+//                   <FormItem>
+//                     <FormLabel>Amount</FormLabel>
+//                     <FormControl>
+//                       <Input {...field} />
+//                     </FormControl>
+//                     <FormDescription>
+//                       Minimum amount of Bitcoin is 0.0005.
+//                     </FormDescription>
+//                     <FormMessage />
+//                   </FormItem>
+//                 )}
+//               />
+//               {/* copy address */}
+//               <div className="">
+//                 <div className="bg-muted w-full py-2 flex justify-center items-center rounded font-mono relative">
+//                   <span className="select-all flex-1 text-center overflow-hidden text-muted-forground text-sm">
+//                     <span className="inline-block max-w-full">
+//                       <span className="inline">{textToCopy.slice(0, 6)}</span>
+//                       <span className="inline text-muted-foreground/60">
+//                         ...
+//                       </span>
+//                       <span className="inline">{textToCopy.slice(-6)}</span>
+//                     </span>
+//                   </span>
+//                   <button
+//                     onClick={handleCopy}
+//                     className="absolute right-3 p-1 rounded hover:bg-background/80 transition-colors"
+//                     title={copied ? "Copied!" : "Copy to clipboard"}
+//                   >
+//                     {copied ? (
+//                       <Check className="h-4 w-4 text-green-600" />
+//                     ) : (
+//                       <Copy className="h-4 w-4 text-muted-foreground hover:text-foreground" />
+//                     )}
+//                   </button>
+//                 </div>
+//               </div>
+//               <Button type="submit" className="w-full">
+//                 Make Deposit
+//               </Button>
+//             </form>
+//           </Form>
+//         </div>
+//         <DrawerFooter className="pt-2 pb-5">
+//           <DrawerClose asChild>
+//             <Button variant="outline">Cancel</Button>
+//           </DrawerClose>
+//         </DrawerFooter>
+//       </DrawerContent>
+//     </Drawer>
+//   );
+// }
+
+// function ProfileForm({ className }: React.ComponentProps<"form">) {
+//   return (
+//     <form className={cn("grid items-start gap-6", className)}>
+//       <div className="grid gap-3">
+//         <Label htmlFor="email">Email</Label>
+//         <Input type="email" id="email" defaultValue="shadcn@example.com" />
+//       </div>
+//       <div className="grid gap-3">
+//         <Label htmlFor="username">Username</Label>
+//         <Input id="username" defaultValue="@shadcn" />
+//       </div>
+//       <Button type="submit">Save changes</Button>
+//     </form>
+//   );
+// }
