2996e57bec257b40f067b4d1c25c76add5cac9f3
matellorson <snr.mn.lordson@gmail.com>
2025-06-29 18:06:34 +0100

    cmt

--- app/dashboard/crypto/deposit/page.tsx
+++ app/dashboard/crypto/deposit/page.tsx
@@ -1,18 +1,30 @@
-import { Card, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import DepositBTC from "./coins/bitcoin";
 import DepositETH from "./coins/ethereum";
 import DepositSOL from "./coins/solana";
 import DepositBNB from "./coins/binance";
 import DepositUSDT from "./coins/usdt";
 import DepositUSDC from "./coins/usdc";
-import DepositXRP from "./xrp";
-import DepositDOGE from "./dodge";
+import DepositXRP from "./coins/xrp";
+import DepositDOGE from "./coins/dodge";
 
 export default function RecieveCoin() {
   return (
     <div className="">
       <Card>
-        <CardContent className="grid grid-cols-2 lg:grid-cols-4 gap-4 justify-center">
+        <CardHeader>
+          <CardTitle>Deposit</CardTitle>
+          <CardDescription>
+            Make a deposit and fund your crypto investment.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-wrap max-w-xl mx-auto justify-center items-center p-3 gap-3">
           <DepositBTC />
           <DepositETH />
           <DepositSOL />
