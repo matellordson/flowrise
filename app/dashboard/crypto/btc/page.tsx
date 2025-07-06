3e3ee855f553bd320b692eb9d963d49eaaad7d3c
Matel Lordson <snr.mn.lordson@gmail.com>
2025-07-06 03:25:24 +0100

    cmt

--- app/dashboard/crypto/btc/page.tsx
+++ app/dashboard/crypto/btc/page.tsx
@@ -1,18 +1,19 @@
 import { TokenBTC } from "@web3icons/react";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, ExternalLink, Globe, Twitter } from "lucide-react";
 import Link from "next/link";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import BTCBalance from "./balance";
 import BTCChart from "./chart";
 import { DepositDrawer } from "../deposit";
 import { SendDrawer } from "../send";
 import { SwapDrawer } from "../swap";
+import { IconBrandTwitter } from "@tabler/icons-react";
 
 export default function BitcoinPage() {
   return (
     <div className="flex h-screen flex-col gap-x-3 gap-y-3 lg:flex-row">
       {/* nav */}
-      <div className="bg-card h-full w-full space-y-5 rounded-xl p-3">
+      <div className="bg-card h-full w-full space-y-5 rounded-xl p-3 lg:w-[50%]">
         <div className="flex items-center gap-x-2">
           <Link href={"/dashboard/crypto"}>
             <ChevronLeft size={30} />
@@ -34,7 +35,51 @@ export default function BitcoinPage() {
         </div>
         <BTCChart />
       </div>
-      <div className="bg-card h-full w-full rounded-xl p-3"></div>
+      <div className="bg-card h-full w-full rounded-xl p-3 lg:w-[50%]">
+        <Tabs defaultValue="about" className="w-[400px]">
+          <TabsList>
+            <TabsTrigger value="about">About</TabsTrigger>
+            <TabsTrigger value="history">History</TabsTrigger>
+          </TabsList>
+          <TabsContent value="about">
+            <p className="text-primary/80 leading-7">
+              Bitcoin (BTC) is the first cryptocurrency built on blockchain
+              technology, also known as a decentralized digital currency that is
+              based on cryptography. Unlike government-issued or fiat currencies
+              such as US Dollars or Euro which are controlled by central banks,
+              Bitcoin can operate without the need of a central authority like a
+              central bank or a company. The decentralized nature allows it to
+              operate on a peer-to-peer network whereby users are able to send
+              funds to each other without going through intermediaries.
+            </p>
+            <a
+              href="https://www.coingecko.com/en/coins/bitcoin"
+              className="flex items-center gap-x-1 pt-2 text-sm text-[var(--brand)]"
+              target="_blank"
+            >
+              Learn more <ExternalLink size={13} />
+            </a>
+
+            <div className="mt-4 flex items-center gap-x-4">
+              <a
+                href="https://bitcoin.org/en/"
+                target="_blank"
+                className="bg-muted text-muted-foreground flex h-fit w-fit items-center justify-center gap-x-1 rounded-xl px-2 py-1 text-sm tracking-wide"
+              >
+                Website <Globe size={15} />
+              </a>
+              <a
+                href="https://x.com/Bitcoin"
+                target="_blank"
+                className="bg-muted text-muted-foreground flex h-fit w-fit items-center justify-center gap-x-1 rounded-xl px-2 py-1 text-sm tracking-wide"
+              >
+                Twitter <Twitter size={15} />
+              </a>
+            </div>
+          </TabsContent>
+          <TabsContent value="history">Coming soon...</TabsContent>
+        </Tabs>
+      </div>
     </div>
   );
 }
