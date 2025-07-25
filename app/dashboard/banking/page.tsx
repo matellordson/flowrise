144809500a87ca3e934d5b2155c1059758456225
Matel Lordson <snr.mn.lordson@gmail.com>
2025-07-25 01:45:59 +0100

    cmt

--- app/dashboard/banking/page.tsx
+++ app/dashboard/banking/page.tsx
@@ -6,16 +6,20 @@ import { useSession } from "next-auth/react";
 import { auth } from "@/auth";
 import BankHistory from "./history";
 
+interface dataType {
+  balance: number;
+}
+
 export default async function Banking() {
   const session = await auth();
 
   async function getBalance() {
     const data =
-      await sql`SELECT balance FROM bank WHERE "user" = ${session?.user?.email} LIMIT 1`;
+      (await sql`SELECT balance FROM bank WHERE "user" = ${session?.user?.email} LIMIT 1`) as dataType[];
     return data;
   }
 
-  const balance: any = await getBalance();
+  const balance = await getBalance();
   return (
     <div className="flex w-full flex-col items-center justify-center gap-3 lg:flex-row">
       <div className="bg-card text-card-forground w-full rounded-xl p-5 lg:h-screen">
